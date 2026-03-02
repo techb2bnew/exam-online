cc82b1d8407b53858df9410cf35164cb1f6065f2
techbase2brand <techbase2brand@gmail.com>
2026-03-02 19:15:54 +0530

    dss

--- middleware.ts
+++ middleware.ts
@@ -35,7 +35,7 @@ export async function middleware(req: NextRequest) {
   }
 }
 
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-}
+// export const config = {
+//   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+// }
 
